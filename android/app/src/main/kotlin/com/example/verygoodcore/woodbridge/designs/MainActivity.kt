d7ce41ebae10d3a3863d50869f33375319761444
Bryce Ferguson <bryce.developer101@gmail.com>
2025-06-27 18:40:06 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/woodbridge/designs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.woodbridge_designs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:woodbridge_designs/app/app.dart';
+import 'package:woodbridge_designs/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
